fa2caeeb55a28546763c41a5b1b681040aa13553
미야 <24730002@g.sewu.ac.kr>
2025-11-30 17:00:35 +0900

    ehfktsk

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -468,7 +468,7 @@ export function Dashboard({ isAuthenticated, userId, onLogout, onShowAuth, locat
   return (
     <div className="relative">
       {/* Main Clock Section - Fixed Height */}
-      <div className="relative min-h-[85vh] flex flex-col">
+      <div className="relative min-h-screen flex flex-col">
         {/* Logo at top */}
         <div className="absolute top-8 left-1/2 transform -translate-x-1/2 z-10">
           <Logo onClick={() => {
@@ -630,7 +630,7 @@ export function Dashboard({ isAuthenticated, userId, onLogout, onShowAuth, locat
         </div>
 
         {/* Bottom Music Controls */}
-        <div className="absolute bottom-24 sm:bottom-20 left-1/2 translate-x-[-50%]">
+        <div className="absolute bottom-32 sm:bottom-28 left-1/2 translate-x-[-50%]">
           {/* Music Controls */}
           <div className="bg-background/80 backdrop-blur-sm border border-border relative rounded-lg h-14 px-6 shadow-lg">
             <div className="flex flex-row gap-5 items-center justify-center h-full">
