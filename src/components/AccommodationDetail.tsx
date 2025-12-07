6c98ce1728334d62600b9cc0708188b58f6c36b5
미야 <24730002@g.sewu.ac.kr>
2025-12-07 23:12:39 +0900

    메시지

--- src/components/AccommodationDetail.tsx
+++ src/components/AccommodationDetail.tsx
@@ -319,7 +319,8 @@ return (
                   <p className="text-muted-foreground leading-relaxed">
                     {accommodation.description}
                   </p>
-                </CardContent>
+                  <div className="h-6"></div>  {/* ← 버튼 아래 여백 추가 */}
+                </CardContent>       
               </Card>
 
               {/* Amenities */}
